bd0b1d92828e1dca8ec2a766054ffb9ac53c2512
Priyanka Gundu <85378322+Priyanka3008@users.noreply.github.com>
2023-01-24 17:26:38 +0530

    Update and rename day-2.py to Inordertraversal.py

---
+++ Inordertraversal.py
@@ -0,0 +1,13 @@
+#Inorder traversal
+class Solution:
+    def inorderTraversal(self,root: Optional[TreeNode]) -> List[int]:
+        out=[]
+        self.inorder(root,out)
+        return out
+    def inorder(self,root,out):
+        if root:
+            self.inorder(root.left,out)
+            out.append(root.val)
+            self.inorder(root.right,out)
+
+            
