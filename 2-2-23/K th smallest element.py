456ce60e90447ae14d04a8f4993bc0dd425003ab
Priyanka Gundu <85378322+Priyanka3008@users.noreply.github.com>
2023-02-02 21:57:14 +0530

    Create K th smallest element.py

---
+++ 2-2-23/K th smallest element.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorder(self,root,l):
+            if root is None:
+                return
+            self.inorder(root.left,l)
+            l.append(root.val)
+            self.inorder(root.right,l) 
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        l=[]
+        self.inorder(root,l)
+        return l[k-1]
